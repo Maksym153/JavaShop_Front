fec5f6b65c983ebe14d169c81b6707e186bfe306
Maksym153 <maksimmonah1@gmail.com>
2023-04-10 21:16:25 -0600

    Add connection to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  useEffect(() => {
+    console.log("use effect App");
+    axios.get("http://localhost:8083/api/categories")
+      .then(resp => {
+        console.log("Server result", resp);
+      });
+  }, []);
+  
   return (
     <h1>Hello React</h1>
   );
